86e422fbc9f796aabd7893767f6cf80b060a9436
samilait <sami.p.laitinen@gmail.com>
2022-04-04 20:03:29 +0300

    puhelinluettelo step4-5 tehty

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,45 +1,19 @@
 import { useState } from 'react'
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '040-1231244'
-     }
-  ])
-
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-
-  const addPerson = (event) => {
-    event.preventDefault()
-    if (persons.filter(p => p.name === newName).length > 0) {
-      window.alert(`${newName} is already added to phonebook`)
-    } else {
-      const personObject = {
-        name: newName,
-        number: newNumber
-      }
-      setPersons(persons.concat(personObject))
-    }
-    setNewName('')
-    setNewNumber('')
-  }
-
-  const handlePersonChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
-
-  const handleNumberChange = (event) => {
-    console.log(event.target.value)
-    setNewNumber(event.target.value)
-  }
-
-
+const Filter = (props) => {
+  const {name, handleFilterPerson} = props
   return (
     <div>
-      <h2>Phonebook</h2>
+      filter shown with <input value={name} onChange={handleFilterPerson} />
+    </div>
+  )
+}
+
+const NewPerson = (props) => {
+  const {addPerson, newName, handlePersonChange, newNumber, handleNumberChange} = props
+  return (
+    <div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handlePersonChange} />
@@ -51,8 +25,87 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
+    </div>
+  )
+}
+
+const Persons = ({ persons }) => {
+  return (
+    <div>
       <h2>Numbers</h2>
       {persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')  
+  const [filterPersons, setFilterPersons] = useState(persons)
+  const [filterName, setFilterName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    console.log('personObject', personObject)
+
+    if (persons.filter(p => p.name.toLowerCase() === newName.toLowerCase()).length > 0) {
+      window.alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat(personObject))
+
+      const copy = [...persons]
+      setFilterPersons(copy.concat(personObject))
+    }
+    setNewName('')
+    setNewNumber('')
+    setFilterName('')
+  }
+
+  const handlePersonChange = (event) => {
+    console.log(event.target.value)
+    console.log('persons', persons)
+    setNewName(event.target.value)
+    setFilterName('')
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterPerson = (event) => {
+    const filterName = event.target.value
+    const filterPersons = persons.filter(person => person.name.toLowerCase().includes(filterName.toLowerCase()))
+    console.log('filter', filterPersons)    
+    setFilterPersons(filterPersons)
+    setFilterName(filterName)
+  }
+
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter name={filterName} handleFilterPerson={handleFilterPerson} />
+      <NewPerson 
+        addPerson={addPerson} 
+        newName={newName}
+        handlePersonChange={handlePersonChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+      <Persons persons={filterPersons} />
     </div>
   )
 
